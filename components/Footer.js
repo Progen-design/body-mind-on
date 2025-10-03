1b703e147dfb69e811f76ef79ea1dc46afabd4cf
Progen-design <janprikopa@gmail.com>
2025-10-03 10:50:10 +0200

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+// components/Footer.js
+export default function Footer(){
+  return (
+    <footer className="site-footer">
+      <div className="container foot">
+        <p>© {new Date().getFullYear()} Body & Mind ON</p>
+        <div className="links">
+          <a href="https://bodyandmindon.cz/obchodni-podminky">Obchodní podmínky</a>
+          <a href="https://bodyandmindon.cz/gdpr">GDPR</a>
+          <a href="mailto:info@bodyandmindon.cz">Kontakt</a>
+        </div>
+      </div>
+    </footer>
+  )
+}

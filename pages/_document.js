8e6eeda961198b657002adee865123adbbc5df91
Progen-design <janprikopa@gmail.com>
2025-10-03 22:23:41 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+
 export default function Document() {
   return (
     <Html lang="cs">
-      <Head />
+      <Head>
+        {/* Odstraněno <link rel="icon" ...> aby se nevolalo favicon.ico */}
+      </Head>
       <body>
         <Main />
         <NextScript />

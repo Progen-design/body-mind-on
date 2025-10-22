0c669df2e12b5fd183e76c8351723c39ea61e503
Progen-design <janprikopa@gmail.com>
2025-10-22 23:08:29 +0200

    Update start.js

--- pages/start.js
+++ pages/start.js
@@ -1,8 +1,8 @@
 import ProgramForm from "../components/ProgramForm";
 
-export default function StartProgram() {
+export default function StartPage() {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-indigo-950 to-black text-white p-8">
+    <div className="min-h-screen bg-gradient-to-b from-green-900 to-black text-white p-8">
       <div className="max-w-4xl mx-auto text-center">
         <h1 className="text-4xl font-bold mb-4 text-green-400">
           START Program – Začni zdarma
@@ -12,7 +12,6 @@ export default function StartProgram() {
           tréninku, jídelníček i regeneraci od našeho AI asistenta.
         </p>
       </div>
-
       <ProgramForm planType="START" />
     </div>
   );

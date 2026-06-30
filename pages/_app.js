293ab5d07244a396263969bc6318e3ee7ced260c
Progen-design <janprikopa@gmail.com>
2026-07-01 00:13:39 +0200

    feat(profile): show device metrics card
    Mount profile device metrics card with connect and sync actions.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 // /pages/_app.js
 import '../styles/globals.css'
 import '../styles/trial-paywall.css'
+import WithingsProfileCard from '../components/profile/WithingsProfileCard'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <WithingsProfileCard />
+    </>
+  )
 }

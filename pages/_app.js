e0068acd0461737b08aab67fec6df42084d9baa2
Progen-design <janprikopa@gmail.com>
2026-07-01 22:57:45 +0200

    fix app layout mount

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,7 @@
 // /pages/_app.js
 import '../styles/globals.css'
 import '../styles/trial-paywall.css'
-import WithingsProfileCard from '../components/profile/WithingsProfileCard'
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <WithingsProfileCard />
-    </>
-  )
+  return <Component {...pageProps} />
 }
